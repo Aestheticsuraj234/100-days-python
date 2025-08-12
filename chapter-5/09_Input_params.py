4c88cccd8614405d20f69c7291e64cf67a27c9f4
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-12 16:21:57 +0530

    ARGS AND KARGS

---
+++ chapter-5/09_Input_params.py
@@ -0,0 +1,15 @@
+chai = "Ginger chai"
+
+
+def prepare_chai(order):
+    print("Preparing" , order)
+
+prepare_chai(chai)
+
+
+def make_chai(tea , milk , sugar):
+    print(tea , milk , sugar)
+    
+make_chai("black" , "milk" , "sugar")
+
+make_chai(tea="black" , milk="milk" , sugar="sugar")
