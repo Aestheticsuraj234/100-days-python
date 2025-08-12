970d4609e322cb6bf13e2a6b5e17e30a390de3a9
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-12 13:45:21 +0530

    walrus

---
+++ chapter-4/09_walrus.py
@@ -0,0 +1,20 @@
+# value = 13
+
+# remainder = value % 5
+
+# if remainder:
+#     print(f"{value} is not divisible by 5")
+
+
+value = 13
+
+if(remainder := value % 5):
+    print(f"{value} is not divisible by 5 remainder is {remainder}")
+    
+available_sizes = ["small" , "medium" , "large"]
+
+if(requested_size := input("Choose your cup size (small , medium , large): ").lower()):
+    if requested_size not in available_sizes:
+        print(f"Sorry , We don't have {requested_size} size available")
+    else:
+        print(f"Your cup is {requested_size}")
