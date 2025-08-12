9be6992e921254d0f65f28b8c18e66ab97873318
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-12 16:13:59 +0530

    08 global

---
+++ chapter-5/08_global.py
@@ -0,0 +1,8 @@
+chai_type = "Elachi"
+def update_order():
+    
+    def kitchen():
+        global chai_type
+        chai_type = "Kesar"
+    kitchen()
+    print("After kitchen update" , chai_type)    
