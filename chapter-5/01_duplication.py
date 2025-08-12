92f682a4eed429a89b63f0bda9c423072336c23f
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-12 15:46:57 +0530

    functions

---
+++ chapter-5/01_duplication.py
@@ -0,0 +1,5 @@
+def print_order(name , chai_type , quantity):
+    print(f"{name} ordered {quantity} chai of {chai_type}")
+    
+
+print_order("Suraj" , "Black" , 2)
---
+++ chapter-5/02_complex.py
@@ -0,0 +1,9 @@
+def fetch_sales():
+    print("Fetching the sales data")
+    
+def filter_valid_sales(sales):
+    print("Filtering the valid sales")
+    
+
+def summarize_data(sales):
+    print("Summarizing the data")        
