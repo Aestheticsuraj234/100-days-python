4048b1244b3caf04cc2f4412bda9836125b5e808
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-13 16:39:23 +0530

    imports

---
+++ chapter-6/main.py
@@ -0,0 +1,11 @@
+# import recipes.flavors 
+
+# print(recipes.flavors.elachai_chai())
+
+from .recipes.flavors import ginger_chai , elachai_chai
+ 
+# from recipes.flavors import elachai_chai , ginger_chai , kadak_chai
+
+# print(elachai_chai())
+# print(ginger_chai())
+# print(kadak_chai()) 
---
+++ chapter-6/recipes/flavors.py
@@ -0,0 +1,10 @@
+def elachai_chai():
+    return "Elachai chai is ready"
+
+
+def ginger_chai():
+    return "Ginger chai is ready"
+
+
+def kadak_chai():
+    return "Kadak chai is ready"
