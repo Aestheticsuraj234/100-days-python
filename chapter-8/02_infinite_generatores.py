c5874add577c90466b05761146480b68906bff27
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-15 10:55:54 +0530

    02 infinite generators

---
+++ chapter-8/02_infinite_generatores.py
@@ -0,0 +1,12 @@
+def infinite_chai():
+    count = 1
+    
+    while True:
+        yield f"Refill #{count}"
+        count += 1
+
+
+refill = infinite_chai()
+
+for _ in range(5):
+    print(next(refill))        
